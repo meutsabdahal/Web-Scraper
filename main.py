ae5b59a654536054c18d42bceebe32ba669c67f8
Utsab Dahal <70103872+meutsabdahal@users.noreply.github.com>
2022-08-08 09:52:14 +0545

    Scrapping Production Website

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 
 # requesting information from a website
-html_text = requests.get('https://www.upwork.com/nx/jobs/search/?q=python&sort=recency').text
-print(html_text)
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from'
+                         '=submit&txtKeywords=Python&txtLocation=').text
+soup = BeautifulSoup(html_text, 'lxml')
+
+job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
+company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+experience_required = job.find('li').text.replace('card_travel', '')
+job_location = job.find('span').text
+skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+print(f'''
+Company Name: {company_name}
+Required Experience: {experience_required}
+Job Location: {job_location}
+Required Skills: {skills}
+''')
 
 
 
