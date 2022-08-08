a480029e0f1f64fe61f9808cccff8306b5c05613
Utsab Dahal <70103872+meutsabdahal@users.noreply.github.com>
2022-08-08 10:20:06 +0545

    Setting up the Project to scrape every 10 minutes

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
+import time
 from bs4 import BeautifulSoup
 import requests
 
-# requesting information from a website
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from'
-                         '=submit&txtKeywords=Python&txtLocation=').text
-soup = BeautifulSoup(html_text, 'lxml')
 
-jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+# function to gather data from website and to store the data
+def find_jobs():
+    # requesting information from a website
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from'
+                             '=submit&txtKeywords=Python&txtLocation=').text
+    soup = BeautifulSoup(html_text, 'lxml')
 
-# for loop to iterate each element
-for job in jobs:
-    company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
-    experience_required = job.find('li').text.replace('card_travel', '')
-    job_location = job.find('span').text
-    skills = job.find('span', class_='srp-skills').text.replace(' ', '')
-    more_info = job.header.h2.a['href']
-    print(f'Company Name: {company_name.strip()}')
-    print(f'Required Experience: {experience_required}')
-    print(f'Job Location: {job_location}')
-    print(f'Required Skills: {skills.strip( )}')
-    print(f'More Info: {more_info}')
-    print('')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+
+    # for loop to iterate each element
+    for job in jobs:
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+        experience_required = job.find('li').text.replace('card_travel', '')
+        job_location = job.find('span').text
+        skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+        more_info = job.header.h2.a['href']
+        print(f'Company Name: {company_name.strip()}')
+        print(f'Required Experience: {experience_required}')
+        print(f'Job Location: {job_location}')
+        print(f'Required Skills: {skills.strip( )}')
+        print(f'More Info: {more_info}')
+        print('')
+
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f'Waiting {time_wait} minutes..')
+        time.sleep(time_wait * 60)
 
 
 
