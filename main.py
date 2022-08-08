f1bff29bef6f88624fbdc7f771ff96b88f8e9035
Utsab Dahal <70103872+meutsabdahal@users.noreply.github.com>
2022-08-08 09:56:49 +0545

    Use of for loop through similar soup.find_all() objects

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?se
                          '=submit&txtKeywords=Python&txtLocation=').text
 soup = BeautifulSoup(html_text, 'lxml')
 
-job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
-company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
-experience_required = job.find('li').text.replace('card_travel', '')
-job_location = job.find('span').text
-skills = job.find('span', class_='srp-skills').text.replace(' ', '')
-print(f'''
-Company Name: {company_name}
-Required Experience: {experience_required}
-Job Location: {job_location}
-Required Skills: {skills}
-''')
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+
+# for loop to iterate each element
+for job in jobs:
+    company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+    experience_required = job.find('li').text.replace('card_travel', '')
+    job_location = job.find('span').text
+    skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+    print(f'''
+    Company Name: {company_name}
+    Required Experience: {experience_required}
+    Job Location: {job_location}
+    Required Skills: {skills}
+    ''')
 
 
 
